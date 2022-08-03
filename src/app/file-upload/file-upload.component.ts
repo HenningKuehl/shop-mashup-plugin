faa7c7b8f8c1638c618340bd7c380d08f9488d06
Henning Kuehl <henning@kuehlonline.de>
2022-08-03 17:17:39 -0400

    add icon and background uploader

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FirebaseStorage} from "@angular/fire/storage";
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {finalize, Observable} from "rxjs";
-import {FileUpload} from "./file-upload";
 import {AngularFireStorage} from "@angular/fire/compat/storage";
 
 @Component({
@@ -10,13 +8,20 @@ import {AngularFireStorage} from "@angular/fire/compat/storage";
   styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent implements OnInit {
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
   private selectedFile: File | null = null;
   uploadPercent: Observable<number | undefined> = new Observable<number>();
   downloadUrl: Observable<string> = new Observable<string>();
+  uploading = false;
+  uploaded = false;
 
   constructor(private storage: AngularFireStorage) { }
 
   ngOnInit(): void {
+  }
+
+  openFileSelection() {
+    this.fileInput.nativeElement.click();
   }
 
   selectFile(event: Event) {
@@ -39,13 +44,18 @@ export class FileUploadComponent implements OnInit {
   }
 
   pushFileToStorage(file: File) {
-    // TODO
-    const mashupId = '';
-    const shopId = '';
+    this.uploading = true;
+    // TODO: set mashupId and shopId
+    const mashupId = 'test_mashup';
+    const shopId = 'test_shop';
     const filePath = `shop-mashup-plugin/${mashupId}/${shopId}/icon.${file.name.split('.').pop()}`;
     const fileRef = this.storage.ref(filePath);
     const uploadTask = this.storage.upload(filePath, file);
-    uploadTask.snapshotChanges().pipe(finalize(() => this.downloadUrl = fileRef.getDownloadURL())).subscribe();
+    uploadTask.snapshotChanges().pipe(finalize(() => {
+      this.downloadUrl = fileRef.getDownloadURL();
+      this.uploading = false;
+      this.uploaded = true;
+    })).subscribe();
     this.uploadPercent = uploadTask.percentageChanges();
   }
 
