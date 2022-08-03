5f5b6e03f9d16bafb58001fed63efc1d243339ee
Henning Kuehl <henning@kuehlonline.de>
2022-08-02 20:52:25 -0400

    authenticate cps helper

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CpsAppHelperService} from "cps-app-helper";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'smp-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private cpsAppHelper: CpsAppHelperService) {
+  }
+
+  ngOnInit() {
+    this.cpsAppHelper.authenticate({
+      appId: environment.applicationId,
+      tappId: chayns.env.site.tapp.id,
+      siteId: chayns.env.site.id,
+      token: chayns.env.user.tobitAccessToken,
+    }).then(authenticated => {
+      console.log(authenticated);
+    }).catch(err => {
+      console.error(err);
+    });
+  }
 }
